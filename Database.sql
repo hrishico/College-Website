832a3b7cf04643177e6146bb849653094b54713e
SarveshNavale <sarveshnavale18@gmail.com>
2025-08-02 13:45:27 +0530

    sample data and corrections

--- Database.sql
+++ Database.sql
@@ -1,29 +1,35 @@
-CREATE DATABASE college_event;
+CREATE DATABASE IF NOT EXISTS college_event;
 USE college_event;
-create table admins_info (
-	id int auto_increment primary key,
-    name varchar(100),
-    ph_no int(10),
-    password varchar(15),
-    email varchar(10)
-);
-ALTER TABLE admins_info
-ADD COLUMN email varchar(20);
 
-create table event_info(
-	event_id int auto_increment primary key,
-    event_name varchar(15),
+create table admins_info (
+    id int auto_increment primary key,
+    name varchar(100),
+    ph_no varchar(10),
+    password varchar(15),
+    email varchar(50)
+);
+
+CREATE TABLE event_info (
+    event_id int auto_increment primary key,
+    event_name varchar(50),
     event_description varchar(1000),
     event_fees int(4)
 );
-create table registered_students(
-	stud_it int auto_increment primary key,
-	first_name varchar(10),
-    last_name varchar(10),
-    student_email varchar(10),
+
+CREATE TABLE registered_students (
+    stud_id int auto_increment primary key,
+    first_name varchar(20),
+    last_name varchar(20),
+    student_email varchar(50),
     phone_no varchar(10),
-    transaction_status varchar(3),
-    reg_event varchar(15)
-    
-    
-)
+    transaction_status varchar(10),
+    reg_event varchar(50)
+);
+
+INSERT INTO admins_info (name, ph_no, password, email)
+VALUES ('Sarvesh Navale', '9763772464', 'password#123', 'sarveshnavale@gmail.com'),
+('Sarvesh Morgoankar', '9828742212', 'password#123', 'sarveshmorgoankar@gmail.com'),
+('Hrishikesh Patil', '9896442212', 'password#123', 'hrishi@gmail.com');
+
+
+
