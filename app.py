4064f3e3646435d703c7b23e1604e9c891dc0485
Collage-project <khotshreyash351@gmail.com>
2025-08-21 11:46:40 +0530

    css fixe

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="hrishi@123",
+    password="shreyash45",
     database="college_event"
 )
 
